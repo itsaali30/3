0c38e7d2fa4b40824884953825db506d669b3280
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2024-11-06 01:38:05 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg); // Broadcast message to all clients
+  });
+});
+
+const port = process.env.PORT || 3000; // Uses environment port if available
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
